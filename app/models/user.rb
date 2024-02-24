5f357cd0204f8e170ad82d4fdda6a33511e87eaf
user-faiz <email2faizm@gmail.com>
2024-02-24 16:35:20 +0530

    fixed linters error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    has_many :categories
-    has_many :expenditures, foreign_key: 'author_id'
-  
-    validates :name, presence: true, length: { maximum: 100 }
-  
-    def total_budget
-      expenditures.sum(:amount)
-    end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :categories
+  has_many :expenditures, foreign_key: 'author_id'
+
+  validates :name, presence: true, length: { maximum: 100 }
+
+  def total_budget
+    expenditures.sum(:amount)
   end
+end
