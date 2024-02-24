a5b716c13726bc075bc6aaa788126d32680eab80
user-faiz <email2faizm@gmail.com>
2024-02-24 22:39:23 +0530

    update application controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,4 @@ class ApplicationController < ActionController::Base
   def splash_screen_controller?
     controller_name == 'splash_screen'
   end
-
-  # def after_sign_out_path_for(resource_or_scope)
-  #     unauthenticated_root_path
-  # end
 end
